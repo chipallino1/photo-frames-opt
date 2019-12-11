cdb1ede351e4d14537d1647ce45db85b7295f3d7
chipallino1 <skorupich00@mail.ru>
2019-12-11 16:50:03 +0300

    File upload started

--- src/main/java/com/ramkiopt/main/controllers/PhotoFramesController.java
+++ src/main/java/com/ramkiopt/main/controllers/PhotoFramesController.java
@@ -17,13 +17,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.websocket.server.PathParam;
+import java.io.IOException;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 @RequestMapping("/photo-frames")
 public class PhotoFramesController {
     private final PhotoFramesStructureService photoFramesStructureService;
@@ -36,8 +40,9 @@ public class PhotoFramesController {
         this.responseService = responseService;
     }
 
-    @PostMapping("/create")
-    public ResponseEntity createPhotoFrame(@RequestBody @Valid PhotoFramesDto dto, BindingResult bindingResult) {
+    @PostMapping(value = "/create")
+    public ResponseEntity createPhotoFrame(@RequestBody @Valid PhotoFramesDto dto,
+                                           BindingResult bindingResult, HttpServletRequest request) throws IOException {
         if (bindingResult.hasErrors()) {
             return responseService.createErrorInfo(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
         }
